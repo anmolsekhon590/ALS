fa4730fdb80b0127ba0101ecf81c440419e07dfe
anmolsekhon590 <54895453+anmolsekhon590@users.noreply.github.com>
2020-08-20 21:49:59 -0400

    added names to inputs

--- insert-records.php
+++ insert-records.php
@@ -13,36 +13,36 @@
     <h1>Insert Records</h1>
     <form action="process-insert-records.php" method="POST">
         
-        <label>Date:  </label>
+        <label>Date: </label>
         <input type="date" name="date">
 
         <br><br>
         <label>Team number: </label>
-        <input type="number" name="">
+        <input type="number" name="teamNumber">
 
         <br><br>
         <label>Name of worker: </label>
-        <input type="text" name="">
+        <input type="text" name="nameOfWorker">
 
         <br><br>
         <label>Area Visited: </label>
-        <input type="text" name="">
+        <input type="text" name="areaVisited">
         
         <br><br>
         <label>Total Houses Visited: </label>
-        <input type="number" name="">
+        <input type="number" name="houseVisited">
         
         <br><br>
         <label>Positive Houses: </label>
-        <input type="number" name="">
+        <input type="number" name="positiveHouses">
 
         <br><br>
         <label>Total Containers Checked: </label>
-        <input type="number" name="">
+        <input type="number" name="containerChecked">
         
         <br><br>
         <label>Group Meetings: </label>
-        <input type="number" name="">
+        <input type="number" name="groupMeetings">
 
         <br><br>
         <input type="submit">
